176254454e333e181cd7334df00edb3fd6e3d998
Mark Lavercombe <mlava@users.noreply.github.com>
2023-02-09 08:47:51 +1100

    Update JSON Webhooks - allow second destination webhook

--- extension.js
+++ extension.js
@@ -13,6 +13,18 @@ const config = {
             description: "The character that marks the beginning or end of a unit of data",
             action: { type: "input", placeholder: ":" },
         },
+        {
+            id: "jsonWH-webhook2",
+            name: "Second Webhook Address (optional)",
+            description: "Place webhook address here",
+            action: { type: "input", placeholder: "Place webhook address here" },
+        },
+        {
+            id: "jsonWH-delimiter2",
+            name: "Second Delimiter (optional)",
+            description: "The character that marks the beginning or end of a unit of data",
+            action: { type: "input", placeholder: ":" },
+        },
     ]
 };
 
@@ -21,27 +33,52 @@ export default {
         extensionAPI.settings.panel.create(config);
 
         window.roamAlphaAPI.ui.commandPalette.addCommand({
-            label: "JSON to Webhook",
-            callback: () => jsonWH()
+            label: "JSON to Webhook #1",
+            callback: () => {
+                const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+                if (uid == undefined) {
+                    alert("Please make sure to focus a block before trying to send data to a webhook");
+                    return;
+                }
+                var which = 1;
+                jsonWH(uid, which);
+            }
+        });
+        window.roamAlphaAPI.ui.commandPalette.addCommand({
+            label: "JSON to Webhook #2",
+            callback: () => {
+                const uid = window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
+                if (uid == undefined) {
+                    alert("Please make sure to focus a block before trying to send data to a webhook");
+                    return;
+                }
+                var which = 2;
+                jsonWH(uid, which);
+            }
         });
 
-        async function jsonWH() {
-            var WebhookDelimiter;
+        async function jsonWH(uid, which) {
+            var WebhookURL, WebhookDelimiter, WebhookURL2, WebhookDelimiter2;
             breakme: {
                 if (!extensionAPI.settings.get("jsonWH-webhook")) {
                     sendConfigAlert();
                     break breakme;
                 } else {
-                    const WebhookURL = extensionAPI.settings.get("jsonWH-webhook");
+                    WebhookURL = extensionAPI.settings.get("jsonWH-webhook");
                     if (!extensionAPI.settings.get("jsonWH-delimiter")) {
                         WebhookDelimiter = ":";
-                        console.log("WebhookDelimiter set to default");
                     } else {
                         WebhookDelimiter = extensionAPI.settings.get("jsonWH-delimiter");
                     }
+                    if (extensionAPI.settings.get("jsonWH-webhook2")) {
+                        WebhookURL2 = extensionAPI.settings.get("jsonWH-webhook2");
+                    }
+                    if (extensionAPI.settings.get("jsonWH-delimiter2")) {
+                        WebhookDelimiter2 = extensionAPI.settings.get("jsonWH-delimiter2");
+                    }
                     var dataString;
-                    const startBlock = await window.roamAlphaAPI.ui.getFocusedBlock()?.["block-uid"];
-                    var thisBlockInfo = window.roamAlphaAPI.data.pull("[:block/string]", [":block/uid", startBlock]);
+
+                    var thisBlockInfo = window.roamAlphaAPI.data.pull("[:block/string]", [":block/uid", uid]);
 
                     var apiCall = `[:find ?ancestor (pull ?block [*])
                 :in $ ?b
@@ -51,67 +88,126 @@ export default {
 
                        [?block :block/parents ?ancestor]]`;
 
-                    var childBlocks = await window.roamAlphaAPI.q(apiCall, startBlock);
+                    var childBlocks = await window.roamAlphaAPI.q(apiCall, uid);
 
                     var blocks = {};
-
-                    if (!WebhookURL.match("ifttt")) {
-                        blocks['parentText'] = thisBlockInfo[":block/string"];
-                    } else {
-                        blocks['value1'] = thisBlockInfo[":block/string"];
+                    if (which == 1) {
+                        if (!WebhookURL.match("ifttt")) {
+                            blocks['parentText'] = thisBlockInfo[":block/string"];
+                        } else {
+                            blocks['value1'] = thisBlockInfo[":block/string"];
+                        }
+                    } else if (which == 2) {
+                        if (!WebhookURL2.match("ifttt")) {
+                            blocks['parentText'] = thisBlockInfo[":block/string"];
+                        } else {
+                            blocks['value1'] = thisBlockInfo[":block/string"];
+                        }
                     }
 
                     var n = 0;
                     for (var i in childBlocks) {
                         dataString = childBlocks[i][1].string;
-                        if (dataString.match(WebhookDelimiter)) {
-                            dataString = dataString.split(WebhookDelimiter);
-                            if (WebhookURL.match("ifttt") && (i < 2)) {
-                                var iftttNumber = parseInt(i) + 2;
-                                blocks['value' + iftttNumber + ''] = encodeURIComponent(dataString[1].trim());
+                        if (which == 1) {
+                            if (dataString.match(WebhookDelimiter)) {
+                                dataString = dataString.split(WebhookDelimiter);
+                                if (WebhookURL.match("ifttt") && (i < 2)) {
+                                    var iftttNumber = parseInt(i) + 2;
+                                    blocks['value' + iftttNumber + ''] = encodeURIComponent(dataString[1].trim());
+                                }
+                                else if (!WebhookURL.match("ifttt")) {
+                                    blocks['' + dataString[0].trim() + ''] = dataString[1].trim();
+                                }
+                            } else {
+                                if (WebhookURL.match("ifttt") && (i < 2)) {
+                                    var iftttNumber = parseInt(i) + 2;
+                                    blocks['value' + iftttNumber + ''] = encodeURIComponent(dataString);
+                                }
+                                else if (!WebhookURL.match("ifttt")) {
+                                    n = n + 1;
+                                    blocks['string' + n] = dataString;
+                                }
                             }
-                            else if (!WebhookURL.match("ifttt")) {
-                                blocks['' + dataString[0].trim() + ''] = dataString[1].trim();
-                            }
-                        } else {
-                            if (WebhookURL.match("ifttt") && (i < 2)) {
-                                var iftttNumber = parseInt(i) + 2;
-                                blocks['value' + iftttNumber + ''] = encodeURIComponent(dataString);
-                            }
-                            else if (!WebhookURL.match("ifttt")) {
-                                n = n + 1;
-                                blocks['string' + n] = dataString;
+                        } else if (which == 2) {
+                            if (dataString.match(WebhookDelimiter2)) {
+                                dataString = dataString.split(WebhookDelimiter2);
+                                if (WebhookURL2.match("ifttt") && (i < 2)) {
+                                    var iftttNumber = parseInt(i) + 2;
+                                    blocks['value' + iftttNumber + ''] = encodeURIComponent(dataString[1].trim());
+                                }
+                                else if (!WebhookURL2.match("ifttt")) {
+                                    blocks['' + dataString[0].trim() + ''] = dataString[1].trim();
+                                }
+                            } else {
+                                if (WebhookURL2.match("ifttt") && (i < 2)) {
+                                    var iftttNumber = parseInt(i) + 2;
+                                    blocks['value' + iftttNumber + ''] = encodeURIComponent(dataString);
+                                }
+                                else if (!WebhookURL2.match("ifttt")) {
+                                    n = n + 1;
+                                    blocks['string' + n] = dataString;
+                                }
                             }
                         }
                     }
 
                     var myHeaders = new Headers();
                     var requestOptions = {};
-                    if (WebhookURL.match("zapier")) {
-                        requestOptions["body"] = JSON.stringify(blocks);
-                    } else if (WebhookURL.match("ifttt")) {
-                        var iftttURL = WebhookURL + "?value1=" + blocks['value1'] + "&value2=" + blocks['value2'] + "&value3=" + blocks['value3'];
-                        requestOptions["mode"] = "no-cors";
-                    } else if (WebhookURL.match("make") || WebhookURL.match("pipedream")) {
-                        myHeaders.append("Content-Type", "application/json");
-                        requestOptions["headers"] = myHeaders;
-                        requestOptions["body"] = JSON.stringify(blocks);
-                    } else {
-                        requestOptions["body"] = JSON.stringify(blocks);
+                    if (which == 1) {
+                        if (WebhookURL.match("zapier")) {
+                            requestOptions["body"] = JSON.stringify(blocks);
+                        } else if (WebhookURL.match("ifttt")) {
+                            var iftttURL = WebhookURL + "?value1=" + blocks['value1'] + "&value2=" + blocks['value2'] + "&value3=" + blocks['value3'];
+                            requestOptions["mode"] = "no-cors";
+                        } else if (WebhookURL.match("make") || WebhookURL.match("pipedream")) {
+                            myHeaders.append("Content-Type", "application/json");
+                            requestOptions["headers"] = myHeaders;
+                            requestOptions["body"] = JSON.stringify(blocks);
+                        } else {
+                            requestOptions["body"] = JSON.stringify(blocks);
+                        }
+                    } else if (which == 2) {
+                        if (WebhookURL2.match("zapier")) {
+                            requestOptions["body"] = JSON.stringify(blocks);
+                        } else if (WebhookURL2.match("ifttt")) {
+                            var iftttURL = WebhookURL2 + "?value1=" + blocks['value1'] + "&value2=" + blocks['value2'] + "&value3=" + blocks['value3'];
+                            requestOptions["mode"] = "no-cors";
+                        } else if (WebhookURL2.match("make") || WebhookURL2.match("pipedream")) {
+                            myHeaders.append("Content-Type", "application/json");
+                            requestOptions["headers"] = myHeaders;
+                            requestOptions["body"] = JSON.stringify(blocks);
+                        } else {
+                            requestOptions["body"] = JSON.stringify(blocks);
+                        }
                     }
                     requestOptions["method"] = "POST";
                     requestOptions["redirect"] = "follow";
 
-                    if (WebhookURL.match("ifttt")) {
-                        await fetch(iftttURL, requestOptions)
-                        console.log("JSON to webhooks - sent")
-                    } else {
-                        const response = await fetch(WebhookURL, requestOptions);
-                        const data = await response.json();
-                        if (response.ok) {
+                    if (which == 1) {
+                        if (WebhookURL.match("ifttt")) {
+                            await fetch(iftttURL, requestOptions)
                             console.log("JSON to webhooks - sent")
                         } else {
-                            console.error(data);
+                            const response = await fetch(WebhookURL, requestOptions);
+                            if (response.ok) {
+                                console.log("JSON to webhooks - sent")
+                            } else {
+                                const data = await response.json();
+                                console.error(data);
+                            }
+                        }
+                    } else if (which == 2) {
+                        if (WebhookURL2.match("ifttt")) {
+                            await fetch(iftttURL, requestOptions)
+                            console.log("JSON to webhooks - sent")
+                        } else {
+                            const response = await fetch(WebhookURL2, requestOptions);
+                            if (response.ok) {
+                                console.log("JSON to webhooks - sent")
+                            } else {
+                                const data = await response.json();
+                                console.error(data);
+                            }
                         }
                     }
                 };
@@ -120,7 +216,10 @@ export default {
     },
     onunload: () => {
         window.roamAlphaAPI.ui.commandPalette.removeCommand({
-            label: 'JSON to Webhook'
+            label: 'JSON to Webhook #1'
+        });
+        window.roamAlphaAPI.ui.commandPalette.removeCommand({
+            label: 'JSON to Webhook #2'
         });
     }
 }
